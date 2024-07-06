bd57bda3ef2c37880a095bba526d9af892af59af
Alexander Iseghohi <admin@rhixe.company>
2024-07-07 00:49:30 +0100

    deploy

--- rhixe_scans/bash/prod.sh
+++ rhixe_scans/bash/prod.sh
@@ -1,22 +1,14 @@
 #!/bin/bash
 
-until docker-compose -f docker-compose-prod.yaml down --remove-orphans -t 0 -v; do
-    echo "Stopping Containers"
+# until docker-compose -f docker-compose.production.yml down --remove-orphans -t 0 -v; do
+#     echo "Stopping Containers"
 
-done
+# done
 
-make clean
+# make clean
 
-sudo rm -r static volumes
-sudo chown -R $USER:$USER .
-until docker-compose -f docker-compose-prod.yaml build --pull; do
-    echo "Building Images"
-done
+# sudo rm -r static volumes
+# sudo chown -R $USER:$USER .
+docker compose -f docker-compose.production.yml build --pull
 
-until docker-compose -f docker-compose-prod.yaml up -d -V; do
-    echo "Starting Containers"
-done
-clear
-make user
-clear
-make debug
+docker-compose -f docker-compose.production.yml up -d -V
