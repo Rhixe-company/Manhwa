f4fe9bb8eb0dca12080a9e712cf572d329517f83
Alexander Iseghohi <admin@rhixe.company>
2024-07-07 01:53:33 +0100

    deploy

--- rhixe_scans/config/settings/base.py
+++ rhixe_scans/config/settings/base.py
@@ -221,7 +221,7 @@ STATIC_URL = "/static/"
 # https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "dist"),
-    os.path.join(BASE_DIR, "components"),
+    # os.path.join(BASE_DIR, "components"),
     os.path.join(BASE_DIR, "static"),
 ]
 # https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#staticfiles-finders
