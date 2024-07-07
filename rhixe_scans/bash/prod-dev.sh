bdb304da19ca869ad5f76ae2be6965e368a20d22
Alexander Iseghohi <admin@rhixe.company>
2024-07-07 03:58:17 +0100

    deploy

--- rhixe_scans/bash/prod-dev.sh
+++ rhixe_scans/bash/prod-dev.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-docker compose -f docker-compose.local.yml down --remove-orphans -t 0 -v
+docker compose -f docker-compose.local.yml down --remove-orphans -t 0
 # make clean
 
 # sudo rm -r static volumes
 # sudo chown -R $USER:$USER .
 docker compose -f docker-compose.local.yml build --pull
 
-docker compose -f docker-compose.local.yml up -d -V
+docker compose -f docker-compose.local.yml up -d
 
 docker compose -f docker-compose.local.yml logs -f
